a299f7f5f9ae02d0beebec0866b60daa242fd21a
Bulichova <bulichova84@gmail.com>
2022-07-24 00:17:03 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from "./images/logo.png"
+import React from 'react'
+import logo from './images/logo.png'
+import Navigation from './components/Navigation'
+
+const Footer =styled.footer`
+`
+
+const navLinks = [
+  { id: '1', label: 'KNOW-HOW' },
+  { id: '2', label: 'O NAS' },
+  { id: '3', label: 'BLOG' },
+  { id: '4', label: 'KONTAKT' },
+]
 
 function App() {
   return (
     <div className="App">
-      <img src = {logo} alt='logo'/>
+      <img src={logo} alt="logo" />
+      <Navigation links={navLinks} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
